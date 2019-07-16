8f2632ed8bcee4ee16527c68babbb725a881407c
Ryuya <volley07@icloud.com>
2019-07-16 12:06:55 +0900

    NN is updatedgit add NN.cpp

--- NN.cpp
+++ NN.cpp
@@ -9,13 +9,9 @@ template<typename T> T sigmoid(T x) {
 
 class N {
 public:
-    N();
     //activation function
-    template<typename T> T h(vector<double>&, vector<double>&);
 
 private:
-    int signal; // 0 or 1
-    float weight;
 };
 
 class NN {
@@ -27,14 +23,4 @@ private:
 int main() {
 
     return 0;
-}
-
-N::N() : signal(0), weight(0) {
-
-}
-
-template<typename T> T N::h(vector<double> &signals, vector<double> &biass) {
-    for(int i(0); i < signals.size(); i++) {
-
-    }
 }
